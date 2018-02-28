a3ee0b46368effc85dc39ca4904ce2d17866ba28
nohlp <nonamehelp@gmail.com>
2018-02-28 17:05:51 +0300

    2 Tasks done

---
+++ Lesson1/Lesson1 Task1/Lesson1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//Азаров Андрей
+/*
+Написать программу “Анкета”. Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). 
+В результате вся информация выводится в одну строчку.
+
+а) используя склеивание;
+б) используя форматированный вывод;
+в) *используя вывод со знаком $.
+*/
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите Ваше имя:");
+            string fname = Console.ReadLine();
+            Console.Write("\nВведите Вашу фамилию:");
+            string sname = Console.ReadLine();
+            Console.Write("\nВведите Ваш возраст:");
+            string age = Console.ReadLine();
+            Console.Write("\nВведите Ваш рост:");
+            string height = Console.ReadLine();
+            Console.Write("\nВведите Ваш вес:");
+            string weight = Console.ReadLine();
+
+            Console.WriteLine("1: Вы ввели имя- "+fname+", фамилия- "+ sname+", возраст- "+ age+", рост- "+ height+", вес- "+weight);
+            Console.WriteLine("2: Вы ввели имя- {0}, фамилия- {1}, возраст- {2}, рост- {3}, вес- {4}",fname,sname,age,height, weight);
+            Console.WriteLine($"3: Вы ввели имя- {fname}, фамилия- {sname}, возраст- {age}, рост- {height}, вес- {weight}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson1/Lesson1 Task1/Lesson1_Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//Азаров Андрей
+/*
+Ввести вес и рост человека. Рассчитать и вывести индекс массы тела(ИМТ) по формуле I=m/(h*h); 
+где m-масса тела в килограммах, h - рост в метрах
+*/
+
+namespace Lesson1_Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("\nВведите Ваш рост:");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nВведите Ваш вес:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваш индекс массы тела(ИМТ) равен {(weight/(height*height)):F}");
+
+            Console.ReadKey();
+        }
+    }
+}
